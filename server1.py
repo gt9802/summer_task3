6fa29e0bbd8c4d7d8929675406a0dc7f0e4496a3
milangt <milangt@localhost.localdomain>
2021-06-11 22:39:13 +0530

    new

--- server1.py
+++ server1.py
@@ -11,8 +11,10 @@ s.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,BUFF_SIZE)
 ip ="192.168.43.222"
 port = 1234
 
+#binding ip and port number
 s.bind((ip,port))
 
+#receives packet from sender and show  using cv2
 def recv():
     while True:
         packet,_ = s.recvfrom(BUFF_SIZE)
@@ -26,22 +28,27 @@ def recv():
             break
 
         
-
+#function to send image stream to sender program 
 def send():
+    #opens webcam
     vid = cv2.VideoCapture(0)
-
-#msg,addr = s.recvfrom(BUFF_SIZE)
+    #set the width t osend image in a single shot of datagram
     WIDTH=400
+    #receives datagram on its socket address
     while(vid.isOpened()):
+        #starts capturing images
         _,frame = vid.read()
+        #resize the image
         frame = imutils.resize(frame,width=WIDTH)
+        #encode the image with a jpeg with a quality of 80%
         encoded,buffer = cv2.imencode('.jpg',frame,[cv2.IMWRITE_JPEG_QUALITY,80])
+        #base64 encoding  
         message = base64.b64encode(buffer)
         s.sendto(message,("192.168.43.148",1235))
 
        
 
-
+#multithreading
 x1 = threading.Thread( target=recv )
 x2 = threading.Thread( target=send )
 
